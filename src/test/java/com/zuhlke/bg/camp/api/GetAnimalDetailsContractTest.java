fc32b59d426b47a204711149aa5532154610432b
Ivanov <ivan.ivanov@zuhlke.com>
2022-04-12 02:09:53 +0300

    Add place holder for consumer contract tests

--- src/test/java/com/zuhlke/bg/camp/api/GetAnimalDetailsContractTest.java
+++ src/test/java/com/zuhlke/bg/camp/api/GetAnimalDetailsContractTest.java
@@ -1,5 +1,17 @@
 package com.zuhlke.bg.camp.api;
 
-@
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(pactVersion = PactSpecVersion.V3)
 public class GetAnimalDetailsContractTest {
+
+    private static final String CONSUMER_NAME = "ZooReception";
+
+    private static final String PROVIDER_NAME = "ZooManager";
+
+    // TODO add your consumer contract tests here
 }
